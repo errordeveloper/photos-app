66165ce2f63fb25ba11a19431e154fe24ee32537
Ilya Dmitrichenko <ilya.dmitrichenko@pachube.com>
2012-08-25 14:35:17 +0100

    Fix `rackup.sh` to not use `bundle exec`, hence avoid double JVM crap!

--- rackup.sh
+++ rackup.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 export FLICKR_API_KEY='574f6b47c0033e517a6965463e7920f7'
 export FLICKR_SHARED_SECRET='fb187e5434e3d47b'
 export FLICKR_USER_ID='31943107@N08'
 
 export JRUBY_OPTS='-Xcext.enabled=true --server --1.9'
 
-exec bundle exec rackup -s thin -p 3000
+./bin/rackup -s thin -p 3000
